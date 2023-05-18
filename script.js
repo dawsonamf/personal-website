3d2dce734f43b9f149b09aaac9469c73b9c7a176
dawsonamf <86806506+dawsonamf@users.noreply.github.com>
2023-05-17 23:14:38 -0400

    some project descriptions

--- script.js
+++ script.js
@@ -160,6 +160,16 @@ function cursorFollow() {
     });
   });
 
+  document.querySelectorAll('.button-link').forEach(item => {
+    item.addEventListener('mouseover', () => {
+      document.querySelector('.cursor-follow').classList.add('cursor-follow-clickable');
+    });
+  
+    item.addEventListener('mouseout', () => {
+      document.querySelector('.cursor-follow').classList.remove('cursor-follow-clickable');
+    });
+  });
+
   document.addEventListener("DOMContentLoaded", function() {
     document.addEventListener("mousemove", function () {
       var cursorContainer = document.querySelector("#cursor-container");
