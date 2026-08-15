b78a121905cc37c24e2abcde3454aec5f70433a3
dawsonamf <86806506+dawsonamf@users.noreply.github.com>
2026-08-15 16:50:09 -0400

    updates

--- js/script.js
+++ js/script.js
@@ -162,6 +162,20 @@
           { action: 'delete', count: 8 },
           { action: 'type', text: "software engineer." },
         ],
+        [
+          { action: 'type', text: "Hi,\nI'm Dawson,\nbuilder." },
+          { action: 'callback', fn: startAnimations },
+          { action: 'pause', duration: 1500 },
+          { action: 'delete', count: 8 },
+          { action: 'type', text: "software engineer." },
+        ],
+        [
+          { action: 'type', text: "Hey,\nI'm Dawson,\nagentic engineer." },
+          { action: 'callback', fn: startAnimations },
+          { action: 'pause', duration: 1500 },
+          { action: 'delete', count: 17 },
+          { action: 'type', text: "software engineer." },
+        ],
         // [
         //   { action: 'type', text: "Hi,\nI'm Dawson,\nsoftware engineer." },
         //   { action: 'callback', fn: startAnimations },
